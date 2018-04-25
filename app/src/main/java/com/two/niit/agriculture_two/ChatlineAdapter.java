27eaefabace52ce58a26726fa42a198e4ec1f023
baiweiqiang <1194798288@qq.com>
2018-04-25 18:24:15 +0800

    改为Chat

--- app/src/main/java/com/two/niit/agriculture_two/ChatlineAdapter.java
+++ app/src/main/java/com/two/niit/agriculture_two/ChatlineAdapter.java
@@ -5,7 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 
-public class ClintonAdapter implements Adapter{
+public class ChatlineAdapter implements Adapter{
     @Override
     public void registerDataSetObserver(DataSetObserver dataSetObserver) {
 
