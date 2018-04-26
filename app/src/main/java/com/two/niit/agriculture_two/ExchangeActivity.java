f5314f3babc71a439b31a8d3f7465cf8c880d112
baiweiqiang <1194798288@qq.com>
2018-04-26 09:32:06 +0800

    新建ExchangeActivity的layout

--- app/src/main/java/com/two/niit/agriculture_two/ExchangeActivity.java
+++ app/src/main/java/com/two/niit/agriculture_two/ExchangeActivity.java
@@ -2,12 +2,25 @@ package com.two.niit.agriculture_two;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.List;
+import java.util.Map;
 
 public class ExchangeActivity extends AppCompatActivity {
+private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange);
+        lv=(ListView)findViewById(R.id.lv);
+        SimpleAdapter simpleAdapter=new SimpleAdapter(this,getData(),R.layout.item,new String[]{"img","tv"},new int[]{R.id.img,R.id.tv});
+
+        lv.setAdapter(simpleAdapter);
     }
+
+
+
 }
